1149db279f13f297da17ea619a2afea3518da401
efdgdfgdf <2571599538@qq.com>
2023-04-05 18:09:19 +0800

    update consts.js

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Air Blog`;
-export const SITE_DESCRIPTION = 'Site Description';
+export const SITE_TITLE = `Lindap👋`;
+export const SITE_DESCRIPTION = '基于Astro 2.0开发的一个苹果风格博客🍎';
 export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_NAME = 'air.lindanpeng.eu.org';
+export const SITE_URL = "https://air.lindanpeng.eu.org";
