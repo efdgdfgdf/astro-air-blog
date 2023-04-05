075c8b2e5f2688ac4a1638c4c411d65137266f13
efdgdfgdf <2571599538@qq.com>
2023-04-05 23:17:19 +0800

    update comment,about,footer

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,7 @@
 export const SITE_TITLE = `Lindap👋`;
 export const SITE_DESCRIPTION = '基于Astro 2.0开发的一个苹果风格博客🍎';
-export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'air.lindanpeng.eu.org';
+export const SITE_EMAIL = 'ldp669896@outlook.com';
+export const SITE_PHONE = "123****4567";
+export const SITE_HOME = "https://home.dp2002.top";
+export const SITE_NAME = 'Danpeng Lin👨🏻‍💻';
 export const SITE_URL = "https://air.lindanpeng.eu.org";
